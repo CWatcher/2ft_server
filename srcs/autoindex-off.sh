77c18c073c05d86e5a24d57e2901f89cd1057486
Carmine Watcher <cwatcher@21-school.ru>
2021-02-24 14:27:48 +0300

    autoindex*.sh =-unlink

--- srcs/autoindex-off.sh
+++ srcs/autoindex-off.sh
@@ -1,4 +1,3 @@
 cd /etc/nginx/snippets
-unlink autoindex.conf
-ln -s autoindex-off.conf autoindex.conf
+ln -sf autoindex-off.conf autoindex.conf
 nginx -s reload
--- srcs/autoindex-on.sh
+++ srcs/autoindex-on.sh
@@ -1,4 +1,3 @@
 cd /etc/nginx/snippets
-unlink autoindex.conf
-ln -s autoindex-on.conf autoindex.conf
+ln -sf autoindex-on.conf autoindex.conf
 nginx -s reload
